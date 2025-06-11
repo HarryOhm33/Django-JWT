c1825ce506d0e3bcb3d99bdefc05abe09925c6dc
Hari Om <hari333333om@gmail.com>
2025-06-11 15:12:30 +0530

    Initial Commit

---
+++ core/user_auth/models.py
@@ -0,0 +1,41 @@
+from mongoengine import Document, EmailField, StringField, BooleanField, DateTimeField
+from datetime import datetime, timedelta
+
+class User(Document):
+    name = StringField(required=True)
+    email = EmailField(required=True, unique=True)
+    password = StringField(required=True)
+    is_verified = BooleanField(default=False)
+    created_at = DateTimeField(default=datetime.now)
+
+    meta = {'collection': 'users'}
+
+class Otp(Document):
+    email = StringField(required=True)
+    otp = StringField(required=True)
+    created_at = DateTimeField(default=datetime.now)
+    expires_at = DateTimeField(default=lambda: datetime.now() + timedelta(minutes=5))
+
+    meta = {
+        'collection': 'otps',
+        'indexes': [
+            {
+                'fields': ['created_at'],
+                'expireAfterSeconds': 300
+            }
+        ]
+    }
+
+class Session(Document):
+    email = StringField(required=True)
+    token = StringField(required=True)
+    created_at = DateTimeField(default=datetime.now)
+
+    meta = {
+        'indexes': [
+            {
+                'fields': ['created_at'],
+                'expireAfterSeconds': 604800  # 7 days in seconds
+            }
+        ]
+    }
