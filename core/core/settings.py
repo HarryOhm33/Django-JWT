304042970eb69689e981901f5d119771f51e28a1
Hari Om <hari333333om@gmail.com>
2025-06-16 12:12:37 +0530

    Made Hosting Ready

--- core/core/settings.py
+++ core/core/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from mongoengine import connect
 from decouple import config
+import os
 
 connect(
     db='Jwt-Auth-Django', 
@@ -31,9 +32,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7e%!x3rhk+)ma1$1v4u8*y703)3-n@)d1d47=w1izilw7!06@l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -140,6 +141,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
 # Default primary key field type
